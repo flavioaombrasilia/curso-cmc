ffeff33fd5a244d81851abadbaf56c99b9a69044
Flavio Augusto <flavioaombrasilia@gmail.com>
2019-01-16 01:19:45 -0300

    iniciamos o projeto

---
+++ src/main/java/com/flavio/cmc/CmcApplication.java
@@ -0,0 +1,14 @@
+package com.flavio.cmc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmcApplication.class, args);
+	}
+
+}
+
