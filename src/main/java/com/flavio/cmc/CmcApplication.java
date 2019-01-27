62f0ed6f05403a6d6d87c1c8a7857406703d8f0d
Flavio Augusto <flavioaombrasilia@gmail.com>
2019-01-27 19:08:58 -0300

    Respositorio Produtos relacionamento categoria gravacao

--- src/main/java/com/flavio/cmc/CmcApplication.java
+++ src/main/java/com/flavio/cmc/CmcApplication.java
@@ -1,7 +1,5 @@
 package com.flavio.cmc;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +8,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.flavio.cmc.domain.Categoria;
+import com.flavio.cmc.domain.Produto;
 import com.flavio.cmc.repositories.CategoriaRepository;
+import com.flavio.cmc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CmcApplication implements CommandLineRunner{
@@ -20,7 +20,10 @@ public class CmcApplication implements CommandLineRunner{
 	}
 
 	@Autowired
-	private CategoriaRepository repo;	
+	private CategoriaRepository catRepo;	
+	
+	@Autowired
+	private ProdutoRepository proRepo;
 	
 	@Override
 	public void run(String... args) throws Exception {
@@ -28,7 +31,20 @@ public class CmcApplication implements CommandLineRunner{
 		Categoria cat01 = new Categoria(null,"Informatica run");
 		Categoria cat02 = new Categoria(null,"Escritório run");
 		
-		repo.saveAll(Arrays.asList(cat01,cat02));
+		Produto p1 = new Produto(null,"Computador",100);
+		Produto p2 = new Produto(null,"Impressora",300);
+		Produto p3 = new Produto(null,"mouse",15600);
+		
+		cat01.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat02.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat01));
+		p2.getCategorias().addAll(Arrays.asList(cat01,cat02));
+		p3.getCategorias().addAll(Arrays.asList(cat01));
+		
+		
+		catRepo.saveAll(Arrays.asList(cat01,cat02));
+		proRepo.saveAll(Arrays.asList(p1,p2,p3));
 		
 		
 	}
