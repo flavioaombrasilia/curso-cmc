fdafee52d80252ef8aad934d0d8a366b31bbc400
Flavio Augusto <flavioaombrasilia@gmail.com>
2019-01-27 22:06:04 -0300

    tratamento de exceção service repository standar err

--- src/main/java/com/flavio/cmc/services/CategoriaServices.java
+++ src/main/java/com/flavio/cmc/services/CategoriaServices.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.flavio.cmc.domain.Categoria;
 import com.flavio.cmc.repositories.CategoriaRepository;
+import com.flavio.cmc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaServices {
@@ -16,7 +17,13 @@ public class CategoriaServices {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Categoria não encontrada. ID: "+id
+					+" , Tipo: "+ Categoria.class.getName() )); 
+		
+		
+		
 	}
 	
 }
