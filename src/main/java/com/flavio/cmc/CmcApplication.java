5c7e787b92de17f15b0f014c6762c04e925e3774
Flavio Augusto <flavioaombrasilia@gmail.com>
2019-01-30 00:00:16 -0300

    entidades Cidade e Estado repositorio classes

--- src/main/java/com/flavio/cmc/CmcApplication.java
+++ src/main/java/com/flavio/cmc/CmcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.flavio.cmc.domain.Categoria;
+import com.flavio.cmc.domain.Cidade;
+import com.flavio.cmc.domain.Estado;
 import com.flavio.cmc.domain.Produto;
 import com.flavio.cmc.repositories.CategoriaRepository;
+import com.flavio.cmc.repositories.CidadeRepository;
+import com.flavio.cmc.repositories.EstadoRepository;
 import com.flavio.cmc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -24,6 +28,12 @@ public class CmcApplication implements CommandLineRunner{
 	
 	@Autowired
 	private ProdutoRepository proRepo;
+	
+	@Autowired
+	private EstadoRepository estRepo;
+	
+	@Autowired
+	private CidadeRepository cidRepo;
 	
 	@Override
 	public void run(String... args) throws Exception {
@@ -45,6 +55,20 @@ public class CmcApplication implements CommandLineRunner{
 		
 		catRepo.saveAll(Arrays.asList(cat01,cat02));
 		proRepo.saveAll(Arrays.asList(p1,p2,p3));
+		
+		Estado e1 = new Estado(null,"Bahia");
+		Estado e2 = new Estado(null,"São Paulo");
+		
+		Cidade c1 = new Cidade(null,"Salvador",e1);
+		Cidade c2 = new Cidade(null,"Feira de Santana",e1);
+		Cidade c3 = new Cidade(null,"São Paulo",e2);
+		
+		e1.getCidades().addAll(Arrays.asList(c1,c2));
+		e2.getCidades().addAll(Arrays.asList(c3));
+		
+		estRepo.saveAll(Arrays.asList(e1,e2));
+		cidRepo.saveAll(Arrays.asList(c1,c2,c3));
+		
 		
 		
 	}
